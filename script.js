b85fbaaf97a832e170990bf6041431cbeb1b6a48
Ridwanullah <aladebaruwa@gmail.com>
2025-05-04 17:38:16 +0100

    add

---
+++ script.js
@@ -0,0 +1,12 @@
+const character = "#";
+let characterArray = [];
+
+
+function createPyramid() {
+    for (i = 0; i < 10; i++) {
+        console.log(character)
+    }
+}
+
+
+console.log(createPyramid())
